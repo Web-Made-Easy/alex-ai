c7708c38852e7ed2727879ec6e8d4ff1f8134dae
Web-Made-Easy <92129937+Web-Made-Easy@users.noreply.github.com>
2024-08-14 06:38:14 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,8 @@ with c1:
     st.title("Alex AI")
     st.write("Your AI Tutor. Powered by Google Generative AI.")
 
-if st.session_state["logged_in"]==True: 
+if st.session_state["logged_in"]==True:
+    log_in_placeholder.empty()
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
